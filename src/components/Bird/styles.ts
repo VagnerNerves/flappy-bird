954eb4e681859e27ca6bd4f5021acabc8464b17b
Vagner Nerves <vagnernervessantos@gmail.com>
2022-12-23 16:37:09 -0300

    feat: Create component Bird.

---
+++ src/components/Bird/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = ({ xBody, yBody, widthBody, heightBody, color }) =>
+  StyleSheet.create({
+    bird: {
+      position: 'absolute',
+      left: xBody,
+      top: yBody,
+      width: widthBody,
+      height: heightBody,
+      resizeMode: 'contain'
+    }
+  })
