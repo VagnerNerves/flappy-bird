96635bef8c8e5bab76c41ccadf4dcefe5a6c75ad
Vagner Nerves <vagnernervessantos@gmail.com>
2022-12-23 18:44:39 -0300

    feat: Implementations physics game.

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -3,6 +3,9 @@ import Matter from 'matter-js'
 
 import Bird from '../components/Bird'
 import Floor from '../components/Floor'
+import Obstacle from '../components/Obstacle'
+
+import { getPipeSizePosPair } from '../utils/ramdom'
 
 const windowHeight = Dimensions.get('window').height
 const windowWidth = Dimensions.get('window').width
@@ -18,9 +21,49 @@ export default () => {
 
   engine.gravity.y = 0.4
 
+  const pipeSizePosA = getPipeSizePosPair()
+  const pipeSizePosB = getPipeSizePosPair(windowWidth * 0.9)
+
   return {
     physics: { engine, world },
     Bird: Bird(world, 'green', { x: 120, y: 400 }, { height: 40, width: 40 }),
+
+    ObstacleTop1: Obstacle(
+      world,
+      'ObstacleTop1',
+      'green',
+      pipeSizePosA.pipeTop.pos,
+      pipeSizePosA.pipeTop.size,
+      true
+    ),
+
+    ObstacleBottom1: Obstacle(
+      world,
+      'ObstacleBottom1',
+      'green',
+      pipeSizePosA.pipeBottom.pos,
+      pipeSizePosA.pipeBottom.size,
+      false
+    ),
+
+    ObstacleTop2: Obstacle(
+      world,
+      'ObstacleTop2',
+      'green',
+      pipeSizePosB.pipeTop.pos,
+      pipeSizePosB.pipeTop.size,
+      true
+    ),
+
+    ObstacleBottom2: Obstacle(
+      world,
+      'ObstacleBottom2',
+      'green',
+      pipeSizePosB.pipeBottom.pos,
+      pipeSizePosB.pipeBottom.size,
+      false
+    ),
+
     Floor: Floor(
       world,
       '#E1D694',
